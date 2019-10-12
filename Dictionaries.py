412e738abd7c531841a9acd031981d51d529c04b
Monty <tuckerjsarah@gmail.com>
2019-10-12 12:14:55 -0400

    dictionary practice

---
+++ Dictionaries.py
@@ -0,0 +1,6 @@
+my_dict = {'key1':'value1', 'key2':'value2'}
+print(my_dict['key1'])
+
+#dictionaries can have different types
+products= {'vegetables':{'root': ['carrots', 'beets', 'garlic', 'potatoes'], 'leafy': ['spinach', 'arugala', 'romaine letuce']}, 'fruit': {'citrus': ['oragnes', 'lemons', 'limes'], 'tropical': ['pineapple', 'mango']}}
+print(products['fruit']['citrus'])
