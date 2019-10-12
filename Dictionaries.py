27831f718ff099d70101b7259a2479df23eeec6d
Monty <tuckerjsarah@gmail.com>
2019-10-12 12:35:38 -0400

    dictionary

--- Dictionaries.py
+++ Dictionaries.py
@@ -4,3 +4,13 @@ print(my_dict['key1'])
 #dictionaries can have different types
 products= {'vegetables':{'root': ['carrots', 'beets', 'garlic', 'potatoes'], 'leafy': ['spinach', 'arugala', 'romaine letuce']}, 'fruit': {'citrus': ['oragnes', 'lemons', 'limes'], 'tropical': ['pineapple', 'mango']}}
 print(products['fruit']['citrus'])
+
+#you can add to the dictionary using assignment
+products['meat'] = {'poultrty': ['chicken', 'turkey'], 'seefood': ['salmon', 'crab', 'shrimp'], 'beef': ['ground beef', 'steak']}
+print(products)
+
+#practice functions associated with dictionaries
+print(f'\n\n{products.keys()}\n')
+print(f'\n{products.values()}\n')
+print(f'\n{products.items()}\n')
+#notice once you call items they are tuples
