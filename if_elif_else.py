20617a464048746a760c7dd8c400a45bc039f0aa
Monty <tuckerjsarah@gmail.com>
2019-10-15 19:19:20 -0400

    if

--- if_elif_else.py
+++ if_elif_else.py
@@ -1,9 +1,15 @@
+#in python indentation is very important. notice the indent for each then portion of the if/then logic
+
+#set variables however you want to get the desired response
 sky = 'blue'
 clouds = False
 
+# if there are blue skies and no clouds then print beautiful day
 if sky == 'blue' and not(clouds):
     print("Wow, it's a beautiful day out!")
+# else if sky is blue but there are clouds then print concern for possible bad weather
 elif sky == 'blue' and clouds:
     print('Uh oh.... it looks a little cloudy. Hopefully it doesnt rain')
+# else you have bad color skies and cloud so print bad weather
 else:
     print('Bad weather today...')
