bbc52dae1dd7563fde5623f3a0c850c19980082b
Monty <tuckerjsarah@gmail.com>
2019-10-15 19:13:37 -0400

    comparison op practice

---
+++ comparisonOperators.py
@@ -0,0 +1,19 @@
+#checking equality
+print(2 == 2)
+print('hello' == 'Hello')
+print(2.0 == 2)
+
+#or inequality
+print( 4 != 5)
+
+# grater than/ less than (or equal to)
+print(2 > 5)
+print(2 < 5)
+print(1 >= 0.5)
+print(1 <= 0.5)
+print( 1 > 0.5 < 0)
+
+#chaining logical operators using and, or, not
+print(f'Here is the and statementd:\n{1 > 0.5 and 0.5 < 0}')
+print(f'Here is the or statement:\n{1 > 0.5 or 0.5 < 0}')
+print(f'Get the opposite with the not statement:\n{not( 1 == 1)}')
---
+++ if_elif_else.py
@@ -0,0 +1,9 @@
+sky = 'blue'
+clouds = False
+
+if sky == 'blue' and not(clouds):
+    print("Wow, it's a beautiful day out!")
+elif sky == 'blue' and clouds:
+    print('Uh oh.... it looks a little cloudy. Hopefully it doesnt rain')
+else:
+    print('Bad weather today...')
