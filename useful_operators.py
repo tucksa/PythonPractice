49850f33acae8cf16498ca61714f841535017d59
Monty <tuckerjsarah@gmail.com>
2019-10-17 19:11:21 -0400

    useful operators

---
+++ useful_operators.py
@@ -0,0 +1,10 @@
+#range(start,stop,step)
+
+#create a list 1-25
+example = []
+for i in range(1,26):
+    example.append(i)
+print(example)
+
+#or cast it to a list using list()
+print(list(range(1,26,2)))
