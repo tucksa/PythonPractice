163de4774e5bddc7a14fa9795f70946ba18899e4
Monty <tuckerjsarah@gmail.com>
2019-10-18 19:03:23 -0400

    enumerate

--- useful_operators.py
+++ useful_operators.py
@@ -8,3 +8,20 @@ print(example)
 
 #or cast it to a list using list()
 print(list(range(1,26,2)))
+
+#Enumerate
+
+#Here is the long hand way of doing it
+index = 0
+for letter in 'abcde':
+    print(f'At index {index} the letter is {letter}')
+    index += 1
+
+#with enumerate 
+word = 'abcde'
+for item in enumerate(word):
+    print(item)
+
+#this returns a tuple so you can unpack it
+for index,letter in enumerate(word):
+    print(f'Index: {index}\nLetter: {letter}')
