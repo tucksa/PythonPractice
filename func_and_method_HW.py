abea7e6c411ddda3ab872386e41e87ace84094f6
Monty <tuckerjsarah@gmail.com>
2019-10-30 16:43:57 -0400

    func hw cont

--- func_and_method_HW.py
+++ func_and_method_HW.py
@@ -19,3 +19,40 @@ def ran_bool(num,low, high):
 
 print(ran_check(10,2,7))
 print(ran_bool(2,2,7))
+
+#Write a Python function that accepts a string and calculates the number of upper case letters and lower case letters.
+def up_low(st):
+    upper_count = 0
+    lower_count = 0
+    for letter in st:
+        if letter.isupper():
+            upper_count += 1
+        elif letter.islower():
+            lower_count += 1
+        else:
+            continue
+            
+    return f'No. of uppercase characters: {upper_count}\nNo. of lowercase characters: {lower_count}'
+
+print(up_low('Hello Mr. Rogers, how are you this fine Tuesday?'))
+
+#Write a Python function that takes a list and returns a new list with unique elements of the first list.
+def unique_list(lst):
+    unique = []
+    for i in lst:
+        if i in unique:
+            continue
+        else:
+            unique.append(i)
+    return unique
+
+print(unique_list([1,1,1,1,3,4,5,5,6,4,6]))
+
+#Write a Python function to multiply all the numbers in a list.
+def mult(num):
+    total = 1
+    for i in num:
+        total *= i 
+    return total
+    
+print(mult([2,2,3]))
