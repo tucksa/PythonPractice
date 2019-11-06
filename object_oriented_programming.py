08382341ce841ff20ea4f48e13e308d9eed98259
Monty <tuckerjsarah@gmail.com>
2019-11-05 19:34:42 -0500

    object oriented programming cont

--- object_oriented_programming.py
+++ object_oriented_programming.py
@@ -20,3 +20,69 @@ my_dog = Dog('Labrador', 'Jessie', 5)
 print(type(my_dog))
 print(my_dog.breed)
 
+#Class object attribute
+class Dog2:
+    species = 'mammal'
+    def __init__(self, breed, color, age):
+        self.breed = breed
+        self.color = color
+        self.age = age
+
+my_dog = Dog2('Labrador', 'black', 7)
+your_dog = Dog2('Poodle', 'white', 2)
+#print both instances and see that species is held 
+print(my_dog.breed)
+print(my_dog.species)
+print(your_dog.breed)
+print(your_dog.species)
+
+#methods- a function ocurring inside a class
+class Dog3:
+    def __init__(self, breed, name):
+        self.breed = breed
+        self.name = name
+    def bark(self):
+        print(f'Woof! My name is {self.name}')
+
+puppy = Dog3('Pit bull', 'Chunk')
+puppy.bark()
+
+#using inheritance to reuse defined class info
+class Animal:
+    def __init__(self):
+        print('animal created')
+    def eat(self):
+        print('I am eating')
+    def sleep(self):
+        print("I am sleeping")
+
+class Dog4(Animal):
+    def __init__(self, breed, name):
+        self.breed = breed
+        self.name = name
+
+#your instance of Dog4 can pull from Animal
+my_dog = Dog4('Lab', 'Tucker')
+my_dog.eat()
+my_dog.sleep()
+
+#Abstract methods
+class Animal2:
+    def __init__(self, name):
+        self.name = name
+    def speak(self):
+        raise NotImplementedError("Subclass must implement this abstract method")
+
+class Dog5(Animal2):
+    def speak(self):
+        return self.name + ' says woof!'
+
+class Cat(Animal2):
+    def speak(self):
+        return self.name + ' says meow!'
+
+fido = Dog5('Fido')
+felix = Cat('Felix')
+
+print(fido.speak())
+print(felix.speak())
